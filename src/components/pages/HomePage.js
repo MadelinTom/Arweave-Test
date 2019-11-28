636e826d47653dc256abbd750c7002ba39eabfbc
Tom Madelin <tom.madelin@gmail.com>
2019-11-28 16:53:21 +0000

    Fixed UI flows

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -12,7 +12,9 @@ export default class HomePage extends React.Component {
         this.state = {
             key: "",
             address: "",
-            status: ""
+            status: "",
+            winston: "",
+            ar: "",
         }
         this.generateWallet = this.generateWallet.bind(this)
         this.getBalance = this.getBalance.bind(this)
@@ -20,41 +22,47 @@ export default class HomePage extends React.Component {
         this.getStatus = this.getStatus.bind(this)
     }
 
-    getAddress = () => {
-        arweave.wallets.jwkToAddress(this.state.key).then((add) => {
+    getAddress = wallet => {
+        arweave.wallets.jwkToAddress(wallet).then((add) => {
             console.log(add)
             this.setState({ address: add })
+        })
+        .then(() => {
+            this.getBalance()
         })
     }
     getBalance = () => {
         arweave.wallets.getBalance(this.state.address).then((balance) => {
             let winston = balance;
             let ar = arweave.ar.winstonToAr(balance)
-
-            console.log(winston);
-            console.log(ar);
+            this.setState({
+                winston: winston,
+                ar: ar
+            })
+            console.log(this.state.winston);
+            console.log(this.state.ar);
         })
     }
     generateWallet = () => {
         arweave.wallets.generate().then((jwt) => {
             console.log(jwt)
-
             this.setState({ key: jwt })
-
         })
+            .then(() => {
+                this.getAddress(this.state.key)
+            })
     }
     getStatus = () => {
         fetch('https://arweave.net/info')
             .then((res) => res.json())
             .then((data) => {
                 console.log(data)
-
                 this.setState({ status: data })
             })
     }
     componentDidMount() {
         this._isMounted = true;
-        this.getStatus() 
+        this.getStatus()
     }
     componentWillUnmount() {
         this._isMounted = false;
@@ -64,19 +72,18 @@ export default class HomePage extends React.Component {
         return (
             <Container>
                 <h1>Arweave Test</h1>
-                <br />
-                <h3>Network status:</h3>
+                <p>Blocks: {this.state.status.blocks}</p>
                 <p>Height: {this.state.status.height}</p>
                 <p>Peers: {this.state.status.peers}</p>
-                <p>Current: {this.state.status.current}</p>
+                <p>Current block: {this.state.status.current}</p>
                 <ButtonGroup>
+                    <Button onClick={this.getStatus}>Get Arweave Status</Button>
                     <Button onClick={this.generateWallet}>Generate Wallet</Button>
-                    <Button onClick={this.getAddress}>Get Address</Button>
                     <Button onClick={this.getBalance}>Get Balance</Button>
-                    <Button onClick={this.getStatus}>Get Status</Button>
                 </ButtonGroup>
-                <p>{this.state.key.n}</p>
                 <p>{this.state.address}</p>
+                <p>Winston: {this.state.winston}</p>
+                <p>Ar: {this.state.ar}</p>
             </Container>
         )
     }
