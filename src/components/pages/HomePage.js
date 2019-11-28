a781323376e77c7150a5b24bd0f79acf0ef098b2
Tom Madelin <tom.madelin@gmail.com>
2019-11-28 09:33:04 +0000

    Fixed blockchain status api call

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,34 +1,27 @@
 import React from "react"
-import { Container } from "reactstrap";
+import { Container, Button, ButtonGroup } from "reactstrap";
 
 import Arweave from "arweave/web";
-const arweave = Arweave.init();
+const arweave = Arweave.init({});
 
 export default class HomePage extends React.Component {
+    _isMounted = false;
+
     constructor(props) {
         super(props)
-
         this.state = {
             key: "",
             address: ""
         }
-
         this.generateWallet = this.generateWallet.bind(this)
         this.getBalance = this.getBalance.bind(this)
         this.getAddress = this.getBalance.bind(this)
     }
 
-    componentDidMount() {
-        this.generateWallet().then((key) => {
-            this.getAddress(key).then((address) => {
-                this.getBalance(address)
-            })
-        })
-    }
     getAddress = key => {
         arweave.wallets.jwkToAddress(key).then((address) => {
+            console.log(address)
             this.setState({ address: address })
-            return address;
         })
     }
     getBalance = address => {
@@ -43,15 +36,39 @@ export default class HomePage extends React.Component {
     generateWallet = () => {
         arweave.wallets.generate().then((jwt) => {
             console.log(jwt)
-            this.setState({ key: jwt })
+            if (this._isMounted) {
+                this.setState({ key: jwt })
+            }
             return jwt;
         })
     }
+    getStatus = () => {
+        fetch('https://arweave.net/info')
+            .then((res) => res.json())
+            .then(function (data) {
+                console.log(data)
+            })
+    }
+    componentDidMount() {
+        this._isMounted = true;
+    }
+    componentWillUnmount() {
+        this._isMounted = false;
+    }
 
     render() {
+
         return (
             <Container>
-                <h1>Test</h1>
+                <h1>Arweave Test</h1>
+                <br />
+                <ButtonGroup>
+                    <Button onClick={this.generateWallet}>Generate Wallet</Button>
+                    <Button onClick={this.getAddress}>Get Address</Button>
+                    <Button onClick={this.getBalance}>Get Balance</Button>
+                    <Button onClick={this.getStatus}>Get Status</Button>
+                </ButtonGroup>
+                <p>{this.state.address}</p>
             </Container>
         )
     }
